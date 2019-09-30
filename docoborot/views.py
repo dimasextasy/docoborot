0f3dab2442ebdd67f4d1e9582a35bd2276a79afc
pichkalev.de <dmitrii.pichkalev@domru.ru>
2019-09-30 14:50:37 +0500

    first hour

---
+++ docoborot/views.py
@@ -0,0 +1,19 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template.loader import get_template
+
+def home_page(request):
+	my_title = "hello universe!"
+	return render(request, "home.html", {"title": my_title})
+
+def about_page(request):
+	my_title = "hello universe!"
+	return render(request, "about.html", {"title": "ABOUT USS"})	
+
+def example_page(request):
+	context = {"title":"Example"}
+	template_name = "hello_world.html"
+	template_obj = get_template(template_name)
+	rendered_obj = template_obj.render(context)
+	return HttpResponse()
+ 
