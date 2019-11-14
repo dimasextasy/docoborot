7a5fcacd0e4a090e51c425f82285fcf5d774fb79
dimasextasy <dimaspichkalev@gmail.com>
2019-11-15 02:58:08 +0500

    КУЧА АПДЕЙТОВ

--- try-django/src/blog/models.py
+++ try-django/src/blog/models.py
@@ -66,14 +66,16 @@ class BlogPost(models.Model): # blogpost_set -> queryset
 
 class Stock(models.Model):
     name  = models.CharField(max_length=120)
+    def __str__(self): return self.name
 
 
 class Product(models.Model): #stockitem_set.all()
     name = models.CharField(max_length=120)
-    title = models.CharField(max_length=120)
     purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
-    selling_field = models.DecimalField(max_digits=10, decimal_places=2)
+    selling_price = models.DecimalField(max_digits=10, decimal_places=2)
     stock_id = models.ForeignKey(Stock, default=1, on_delete=models.CASCADE, null=False)
+    def __str__(self): return self.name
+    def get_id(self): return self.id
 
 
 class Partner(models.Model): #stockitem_set.all()
@@ -81,19 +83,21 @@ class Partner(models.Model): #stockitem_set.all()
     surname = models.CharField(max_length=120)
     patronymic  = models.CharField(max_length=120)
     company_name   = models.CharField(max_length=120)
+    def __str__(self): return self.company_name
 
 
 class DealType(models.Model):
     value = models.CharField(max_length=120)
+    def __str__(self): return self.value
 
 
 class Deal(models.Model):
     partner_id = models.ForeignKey(Partner, default=1, on_delete=models.CASCADE, null=False)
     date = models.DateField()
-    deal_type   = models.ForeignKey(DealType, default=1, on_delete=models.CASCADE, null=False)
+    deal_type = models.ForeignKey(DealType, default=1, on_delete=models.CASCADE, null=False)
 
 
 class DealProduct(models.Model):
     deal_id = models.ForeignKey(Deal, default=1, on_delete=models.CASCADE, null=False)
     product_id = models.ForeignKey(Product, default=1, on_delete=models.CASCADE, null=False)
-    count = models.DecimalField(max_digits=10, decimal_places=2)
+    count = models.IntegerField()
